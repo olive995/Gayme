69af5ed857eb7de9327dd287d0271ddabbc7d218
Olivia Blackwood <aliengravyy@gmail.com>
2017-05-19 20:58:41 -0700

    boop

--- __main__.py
+++ __main__.py
@@ -1,35 +1,69 @@
 import pygame
 import sys
+import random
+
 
 pygame.init()
 
 
-display_width = 800
-display_height = 600
+display_width = 1280
+display_height = 720
 
 gameDisplay = pygame.display.set_mode((display_width,display_height))
 pygame.display.set_caption('Gayme')
 
 black = (0,0,0)
 white = (255,255,255)
-cat_width = 73
-cat_height = 73
+red = (255,0,0)
+
+cat_width = 25
+cat_height = 25
 clock = pygame.time.Clock()
 catImg = pygame.image.load('cat.png')
 catScene = pygame.image.load('catscene.jpg')
+pickle = pygame.image.load('pickle.png')
+
+def things(thingx, thingy, thingw, thingh, image):
+    gameDisplay.blit(pickle, (thingx,thingy,thingw,thingh))
 
 def cat(x,y):
 
     gameDisplay.blit(catScene, ((x/(display_width)), (y/(display_height))))
     gameDisplay.blit(catImg, (x,y))
 
+def text_objects(text, font):
+    textSurface = font.render(text, True, red)
+    return textSurface, textSurface.get_rect()
+
+def message(text):
+    largeText = pygame.font.Font('freesansbold.ttf',80)
+    TextSurf, TextRect = text_objects(text, largeText)
+    TextRect.center = ((display_width/2),(display_height/2))
+    gameDisplay.blit(TextSurf, TextRect)
+
+    pygame.display.update()
+
+
+    pygame.time.delay(500)
+
+    game_loop()
+
+def crash():
+    message("*blep*")
+
 def game_loop():
 
-    x =  (display_width * 0.45)
-    y = (display_height * 0.8)
+    x = (display_width * 0.46)
+    y = (display_height * 0.45)
     x_change = 0
     y_change = 0
     gameExit = False
+
+    thing_startx = random.randrange(0, display_width)
+    thing_starty = -600
+    thing_speed = 7
+    thing_width = 50
+    thing_height = 50
 
 
 
@@ -42,31 +76,40 @@ def game_loop():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
                     pygame.transform.flip(gameDisplay, 180, 0)
-                    x_change = -5
+                    x_change = -10
                 elif event.key == pygame.K_UP:
-                    y_change = -5
+                    y_change = -10
                 elif event.key == pygame.K_DOWN:
-                    y_change = 5
+                    y_change = 10
                 elif event.key == pygame.K_RIGHT:
-                    x_change = 5
+                    x_change = 10
                 elif event.key -- pygame.K_ESCAPE:
                     pygame.quit()
 
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                     x_change = 0
+
                 elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
                     y_change = 0
 
         x += x_change
         y += y_change
 
+        gameDisplay.fill(white)
         cat(x,y)
+        things(thing_startx, thing_starty, thing_width, thing_height, pickle)
+        thing_starty += thing_speed
+
 
         if x > display_width - cat_width or x < 0:
-            x_change = 0
+            crash()
         if y > display_height - cat_height or y < 0:
-            y_change = 0
+            crash()
+
+        if thing_starty > display_height:
+            thing_starty = 0 - thing_height
+            thing_startx = random.randrange(0,display_width)
 
         pygame.display.update()
         clock.tick(60)
